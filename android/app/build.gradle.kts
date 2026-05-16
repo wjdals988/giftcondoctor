36fda7b804f2cd2f81d1551c9187c8f83c133bed
NT11342 <jeongmin2@nts-corp.com>
2026-05-17 00:36:37 +0900

    Fix Android CLI build configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,6 +36,17 @@ android {
         compose = true
         buildConfig = true
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
--- android/app/src/main/java/com/giftcondoctor/app/ui/GiftcondoctorApp.kt
+++ android/app/src/main/java/com/giftcondoctor/app/ui/GiftcondoctorApp.kt
@@ -11,8 +11,8 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.navDeepLink
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navDeepLink
 import androidx.navigation.navArgument
 import com.giftcondoctor.app.ui.screens.AddCouponScreen
 import com.giftcondoctor.app.ui.screens.CouponDetailScreen
